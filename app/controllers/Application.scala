5dedd5736465a05dd5ac1543ab8f3bc582fac12f
pussinboots <pussinboots666@googlemail.com>
2014-05-19 17:08:35 +0200

    - initial commit

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -23,15 +23,67 @@ import scala.slick.lifted.ColumnOrdered
 import DB.dal.profile.simple.{Query => SlickQuery}
 import java.sql.Timestamp
 
+object Helpers {
+
+  import DB.dal._
+  import DB.dal.profile.simple._
+
+ implicit class QueryExtensionSort[E, T <: Table[E]](val query: Query[T, E]) {
+    def sorts(sort: String, direction: String) = {
+      var _query = query
+      sort.reverse.split(" ").foreach { s =>
+        println("sort " +  s.reverse + " " + direction)
+        _query = sortKey(s.reverse, direction)
+      }
+      _query
+    }
+
+    private def sortKey(sort: String, direction: String): Query[T, E] = {
+      query.sortBy(table =>
+        direction match {
+          case "asc" =>
+            println("sort2 " + sort + " asc")
+            table.column[String](sort).asc
+          case "desc" =>
+            println("sort2 " + sort + " desc")
+            table.column[String](sort).desc
+        })
+    }
+  }
+
+  implicit class QueryExtensionSort2[E, T <: Table[E]](val query: Query[(T, _),(E, Option[String])]) {
+    def sorts(sort: String, direction: String) = {
+      var _query = query
+      sort.reverse.split(" ") foreach { s =>
+        _query = sortKey(s.reverse, direction)
+      }
+      _query
+    }
+
+    private def sortKey(sort: String, direction: String): Query[(T, _),(E, Option[String])] = {
+      query.sortBy(table =>
+        direction match {
+          case "asc" => table._1.column[String](sort).asc
+          case "desc" => table._1.column[String](sort).desc
+        })
+    }
+  }
+}
+
 object Application extends Controller {
+
   import DB.dal._
   import DB.dal.profile.simple._
 
   implicit val balanceWrites = Json.writes[Balance]
   implicit val stockWrites = Json.writes[Stock]
+
+  import Helpers._
+
   implicit def tuple2Writes[A, B](implicit aWrites: Writes[A], bWrites: Writes[B]): Writes[Tuple2[A, B]] = new Writes[Tuple2[A, B]] {
-	def writes(tuple: Tuple2[A, B]) = JsArray(Seq(aWrites.writes(tuple._1), bWrites.writes(tuple._2)))
+    def writes(tuple: Tuple2[A, B]) = JsArray(Seq(aWrites.writes(tuple._1), bWrites.writes(tuple._2)))
   }
+
   implicit val stockAndSymbolWrites = Json.writes[StockJson]
 
   def index = Action {
@@ -40,37 +92,24 @@ object Application extends Controller {
 
   def findBalances(name: String, sort: String, direction: String, items: Int) = Action { request =>
     DB.db withSession {
-        def query = if (name.length > 0) Balances.findByName(name) else Balances.findAll()
-        println(sorts(query, sort, direction).take(items).selectStatement)
-    	def json = sorts(query, sort, direction).take(items).list()
-    	Ok(Json.stringify(Json.toJson(json))) as ("application/json")
+      def query = if (name.length > 0) Balances.findByName(name) else Balances.findAll()
+      println(query.sorts(sort, direction).take(items).selectStatement)
+      def json = query.sorts(sort, direction).list()//.take(items).list()
+      Ok(Json.stringify(Json.toJson(json))) as ("application/json")
     }
   }
 
-  def findStocks(name: String, date:Long, sort: String, direction: String, items: Int, page: Int) = Action { request =>
+  def findStocks(name: String, date: Long, sort: String, direction: String, items: Int, page: Int) = Action { request =>
     DB.db withSession {
-        var query = if (name.length > 0) Stocks.findByName(name) else Stocks.findAllWithJoin()
-	query = if (date > -1) query.filter(_._1.date===new Timestamp(date)) else query
-	//FIXME sorting is not working at the moment        
-	println(query.drop(items * (page-1)).take(items).selectStatement)
-    	def json = query.drop(items * (page-1)).take(items).list()
-	def result = json map {case (stock: Stock, symbol: Option[String]) =>
-		StockJson(stock.id.get, stock.name, stock.value, symbol.getOrElse(""), stock.date.get)	
-	}    	
-	Ok(Json.stringify(Json.toJson(result))) as ("application/json")
+      var query = if (name.length > 0) Stocks.findByName(name) else Stocks.findAllWithJoin()
+      query = if (date > -1) query.filter(_._1.date === new Timestamp(date)) else query
+      //FIXME sorting is not working at the moment
+      println(query.drop(items * (page - 1)).take(items).selectStatement)
+      def json = query.sorts(sort, direction).drop(items * (page - 1)).take(items).list()
+      def result = json map { case (stock: Stock, symbol: Option[String]) =>
+        StockJson(stock.id.get, stock.name, stock.value, symbol.getOrElse(""), stock.date.get)
+      }
+      Ok(Json.stringify(Json.toJson(result))) as ("application/json")
     }
   }
-
-  def sorts[E <: Table[_], T](query: SlickQuery[E, T], sort: String, direction: String) = {
-     var _query = query
-     sort.reverse.split(" ") foreach{s=>	
-	_query = _query.sortBy(sortKey(_, s.reverse, direction))
-     }
-     _query		
-  }
-
-  def sortKey[E <: Table[_]](e: E, sort: String, direction: String): ColumnOrdered[_] = direction match {
-	    case "asc" => e.column[String](sort).asc
-	    case "desc" => e.column[String](sort).desc
-	  }
 }
