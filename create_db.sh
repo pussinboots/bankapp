4158f07365fafc2c609faaa888b971b99397fe8c
pussinboots <pussinboots666@googlemail.com>
2014-05-21 11:08:49 +0200

    - implements ModelSpec to test the database interaction (not finsihed \n - added create_db.sh script which perform tables creation and insert the default stock symbols if some of them not exists \n - prepare for travis)

---
+++ create_db.sh
@@ -0,0 +1,3 @@
+#export sparkasse_password=<password>
+#export sparkasse_username=<username>
+sbt "run-main app.tools.DBMigration"
---
+++ test/org/stock/manager/ModelSpec.scala
@@ -0,0 +1,34 @@
+package org.stock.manager
+
+import org.specs2.mutable.Specification
+import play.api.test._
+import play.api.test.Helpers._
+import scala.slick.driver.H2Driver
+import model.DB
+import model.DAL
+import scala.slick.session.Database
+import Database.threadLocalSession
+import play.api.test.PlaySpecification
+
+class ModelSpec extends PlaySpecification {
+  val db = DB.getSlickHSQLDatabase()
+  val dao = new DAL(H2Driver)
+  import dao._
+  import dao.profile.simple._
+
+  "DB test" should {
+    "create tables" in {
+      db withSession {
+        dao.create
+        0 must beEqualTo(0)
+      }
+    }
+    "insert symbols" in {
+      db withSession {
+	Symbols.insertIfNotExists("COMMERZBANK AG", "CBK.DE").symbol must beEqualTo("CBK.DE")
+        Symbols.insertIfNotExists("E.ON SE NA", "EOAN.DE").symbol must beEqualTo("EOAN.DE")
+        Symbols.insertIfNotExists("LBB-PRIVATDEPOT 3 (B)", "A1JSHG.DE").symbol must beEqualTo("A1JSHG.DE")
+      }
+    } 
+  }
+}
