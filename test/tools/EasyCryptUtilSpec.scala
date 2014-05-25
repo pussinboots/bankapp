bde88c4cc7802e92f8d13c524cefb6a88811ab6e
pussinboots <pussinboots666@googlemail.com>
2014-05-25 03:25:16 +0200

    - fixed compile error after make EasyCryptUtil implicit

--- test/tools/EasyCryptUtilSpec.scala
+++ test/tools/EasyCryptUtilSpec.scala
@@ -7,13 +7,14 @@ import javax.crypto.KeyGenerator
 import sun.misc.BASE64Encoder
 import sun.misc.BASE64Decoder
 import javax.crypto.spec.SecretKeySpec
+import tools.EasyCryptUtil._
 
 class EasyCryptUtilSpec extends PlaySpecification {
   
   "encryption" should {
     "aes encryption with specific key" in {
 	sys.props.+=("aes_key" -> "16rdKQfqN3L4TY7YktgxBw==")
-	val encrypted =	EasyCryptUtil.encrypt("Hallo AxxG-Leser")
+	val encrypted =	"Hallo AxxG-Leser".encrypt
         encrypted.key must beEqualTo("16rdKQfqN3L4TY7YktgxBw==")
 	encrypted.value must beEqualTo("Hallo AxxG-Leser")
 	encrypted.encrypted must beEqualTo("BGqqWc8RB0z5AqGjgAoWyWwrvhIHY1vJCR/MNy3a2Os=")  
