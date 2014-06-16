2cbce4e2528e45c1e433390d54911b3bbaf8bc14
Frank <pussinboots666@googlemail.com>
2014-06-16 21:13:33 +0200

    - reduced test code by using Datatable pattern
    Description of Datatables look here http://etorreborre.github.io/specs2/guide/org.specs2.guide.Matchers.html#DataTables.

--- test/integration/E2ETestGlobalSpec.scala
+++ test/integration/E2ETestGlobalSpec.scala
@@ -1,105 +1,74 @@
 package integration
 
-import play.api.libs.ws._
-import play.api.test._
-import play.api.test.Helpers._
-import scala.slick.session.Database
-import Database.threadLocalSession
 import model.{DB, DAL, Balance, Stock}
 import test.E2ETestGlobal
 import play.api.Play
 import play.api.Play.current
+import play.api.test._
+import play.api.test.Helpers._
+import org.specs2.execute.AsResult
+import org.specs2.matcher.DataTables
+import org.specs2.mutable.Before
 
-class E2ETestGlobalSpec extends PlaySpecification {
+import scala.slick.session.Database
+import Database.threadLocalSession
+
+
+class E2ETestGlobalSpec extends PlaySpecification with DataTables {
+  sys.props.+=("Database" -> "h2")
+  import DB.dal._
+  import DB.dal.profile.simple._
+      
   val googleId  = "test googleId"
   val googleIdEnc  = "test googleid encrypted"
-  "application configuration for e2e" should {
-    "all test data for googleid test googleId" should {
-      "user account exists" in { 
-        running(FakeApplication()) {
-          DB.db withSession {
-            import DB.dal._
-            import DB.dal.profile.simple._
-            E2ETestGlobal.onStart(Play.application)
-            DB.db withSession {
-              val q = UserAccounts.findByGoogle(googleId)
-              val userAccount = q.first
-              userAccount.googleId must beEqualTo(googleId)
-              userAccount.id must beEqualTo(Some(1))
-            }
+
+  def around[T: AsResult](f: => T) = {
+    running(FakeApplication()) {
+      E2ETestGlobal.onStart(Play.application)
+      DB.db withSession {
+        AsResult(f)
+      }
+    }
+  }
+
+  "teat data configuration for e2e" should {
+    "all test users exists" in { 
+        "id" | "googleId"   |
+        1    ! googleId     |
+        4    ! googleIdEnc  |> { (id, gid)=>around{
+            val q = UserAccounts.findByGoogle(gid)
+            val userAccount = q.first
+            userAccount.googleId must beEqualTo(gid)
+            userAccount.id must beEqualTo(Some(id))
           }
         }
-      }
-      "balances exists" in {
-        running(FakeApplication()) {
-          import DB.dal._
-          import DB.dal.profile.simple._
-          E2ETestGlobal.onStart(Play.application)
-          DB.db withSession {
-            val balances = Balances.findAll()(googleId).sortBy(_.date.asc).list
-            balances.length must beEqualTo(2)
-            balances(0).name must beEqualTo("Girokonto")
-            balances(1).name must beEqualTo("Total")
-            balances(0).value must beEqualTo("281.21")
-            balances(1).value must beEqualTo("100.01")
-            balances(0).googleId.get must beEqualTo(googleId)
-            balances(1).googleId.get must beEqualTo(googleId)
-          }
-        }
-      }
-      
-      "stocks exists" in {
-        running(FakeApplication()) {
-          import DB.dal._
-          import DB.dal.profile.simple._
-          E2ETestGlobal.onStart(Play.application)
-          DB.db withSession {
-            val stocks = Stocks.findAll()(googleId).sortBy(_._1.date.asc).list
-            stocks.length must beEqualTo(2)
-            stocks(0)._1.name must beEqualTo("Aktie 2")
-            stocks(1)._1.name must beEqualTo("Aktie 1")
-            stocks(0)._1.value must beEqualTo("28.21")
-            stocks(1)._1.value must beEqualTo("11.01")
-            stocks(0)._1.googleId.get must beEqualTo(googleId)
-            stocks(1)._1.googleId.get must beEqualTo(googleId)
-          }
+    }
+    "all balances exists" in {
+      "googleId"       |  "idx" | "name"                       |  "value"                    |
+      googleId        !!   0    ! "Girokonto"                 !! "281.21"                    |
+      googleId        !!   1    ! "Total"                     !! "100.01"                    |
+      googleIdEnc     !!   0    ! "0uCwmlNo9baOU8Sp8CXqWg=="  !! "/Y6EQMQ3rSeOhtqhwuEZLA=="  |
+      googleIdEnc     !!   1    ! "0uCwmlNo9baOU8Sp8CXqWg=="  !! "yta9QMvvmrhKvqtFw+OeTg=="  |> 
+      { (gid, idx, name, value)=> around {
+          val balances = Balances.findAll()(gid).sortBy(_.date.asc).list
+          balances.length must beEqualTo(2)
+          balances(idx).name must beEqualTo(name)
+          balances(idx).value must beEqualTo(value)
+          balances(idx).googleId.get must beEqualTo(gid)
         }
       }
     }
-
-
-    "all test data for googleid test googleid encrypted" should {
-      "check that balances for the googleid encrypted exists" in {
-        running(FakeApplication()) {
-          import DB.dal._
-          import DB.dal.profile.simple._
-          E2ETestGlobal.onStart(Play.application)
-          DB.db withSession {
-            val balances = Balances.findAll()(googleIdEnc).sortBy(_.date.asc).list
-            balances.length must beEqualTo(2)
-            balances(0).name must beEqualTo("0uCwmlNo9baOU8Sp8CXqWg==")
-            balances(1).name must beEqualTo("0uCwmlNo9baOU8Sp8CXqWg==")
-            balances(0).value must beEqualTo("/Y6EQMQ3rSeOhtqhwuEZLA==")
-            balances(1).value must beEqualTo("yta9QMvvmrhKvqtFw+OeTg==")
-            balances(0).googleId.get must beEqualTo(googleIdEnc)
-            balances(1).googleId.get must beEqualTo(googleIdEnc)
-          }
-        }
-      }
-
-      "check that user account for googleid encrypted exists" in {
-        running(FakeApplication()) {
-          DB.db withSession {
-            import DB.dal._
-            import DB.dal.profile.simple._
-            E2ETestGlobal.onStart(Play.application)
-            DB.db withSession {
-              val q = UserAccounts.findByGoogle(googleIdEnc)
-              val userAccount = q.first
-              userAccount.googleId must beEqualTo(googleIdEnc)
-              userAccount.id must beEqualTo(Some(4))
-            }
-          }
+    
+    "all stocks exists" in {
+      "googleId"       |  "idx" | "name"                        |  "value"                   |
+      googleId        !!   0    ! "Aktie 2"                    !! "28.21"                    |
+      googleId        !!   1    ! "Aktie 1"                    !! "11.01"                    |> 
+      { (gid, idx, name, value)=> around {
+          val stocks = Stocks.findAll()(gid).sortBy(_._1.date.asc).list
+          stocks.length must beEqualTo(2)
+          stocks(idx)._1.name must beEqualTo(name)
+          stocks(idx)._1.value must beEqualTo(value)
+          stocks(idx)._1.googleId.get must beEqualTo(gid)
         }
       }
     }
