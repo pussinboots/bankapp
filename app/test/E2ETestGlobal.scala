9e72a00db21af382731b3ee5886f62b824b48593
Frank <pussinboots666@googlemail.com>
2014-06-17 12:17:18 +0200

    - to get less logging during test phase implements clearProperty for the c3p0 logging system preoprties

--- app/test/E2ETestGlobal.scala
+++ app/test/E2ETestGlobal.scala
@@ -7,10 +7,22 @@ import global.Global
 object E2ETestGlobal extends GlobalSettings {
     override def onStart(app: Application) {
       sys.props.+=("Database" -> "h2")
+      val enableDBSSL = app.configuration.getBoolean("enableDBSSL").getOrElse(true)
+      val enablePoolLogging = app.configuration.getBoolean("enablePoolLogging").getOrElse(false)
+      if(!enableDBSSL) {
+        System.clearProperty("javax.net.ssl.keyStore")
+        System.clearProperty("javax.net.ssl.keyStorePassword")
+        System.clearProperty("javax.net.ssl.trustStore")
+        System.clearProperty("javax.net.ssl.trustStorePassword")
+      }
+      if(!enablePoolLogging) {
+        System.clearProperty("com.mchange.v2.log.MLog")
+        System.clearProperty("com.mchange.v2.log.FallbackMLog.DEFAULT_CUTOFF_LEVEL")
+      }
       DB.db withSession {
       	SetupTestDatabase.insertTestData()
         SetupTestDatabase.insertE2ETestData()
       }
-      Logger.info("Application for e2e test has started")
+      Logger.debug("Application for e2e test has started")
     }
 }
