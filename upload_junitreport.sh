38f0848358b3ff11eb73d7816dd5393a34ea9339
Frank <pussinboots666@googlemail.com>
2014-06-18 19:09:22 +0200

    - improved junit upload script [skip ci]

--- upload_junitreport.sh
+++ upload_junitreport.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+owner=pussinboots
+project=bankapp
 FILES=./target/test-reports/*
+#upload play junit reports
 for f in $FILES
 do
   echo "Processing $f file..."
-  # take action on each file. $f store current file name
-  curl -H "Content-Type:application/xml" -X POST --data-binary @$f http://unitcover.herokuapp.com/api/pussinboots/bankapp
+  curl -H "Content-Type:application/xml" -X POST -d @$f http://unitcover.herokuapp.com/api/$owner/$project
 done
 
-curl -H "Content-Type:application/xml" -X POST -d @test-result.xml http://unitcover.herokuapp.com/api/pussinboots/bankapp
+curl -H "Content-Type:application/xml" -X POST -d @test-result.xml http://unitcover.herokuapp.com/api/$owner/$project
+
 
