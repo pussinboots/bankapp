014039cf16629771a6d713907917ec0f5e6ad4a6
Frank <pussinboots666@googlemail.com>
2014-06-18 19:23:47 +0200

    - fixed broken test

--- test/unit/org/stock/manager/DBSpec.scala
+++ test/unit/org/stock/manager/DBSpec.scala
@@ -10,13 +10,13 @@ class DBSpec extends PlaySpecification {
   "DB" should {
     "given no jdbc url as CLEARDB_DATABASE_URL property return local jdbc url" in {
       val dburl = DB.parseDbUrl()
-      dburl._1 must beEqualTo("jdbc:mysql://127.0.0.1:3306/heroku_9852f75c8ae3ea1?autoReconnectForPools=true&reconnect=true&useSSL=true&useUnicode=yes&characterEncoding=UTF-8")
+      dburl._1 must beEqualTo("jdbc:mysql://127.0.0.1:3306/heroku_9852f75c8ae3ea1?useSSL=true&autoReconnectForPools=true&reconnect=true&useUnicode=yes&characterEncoding=UTF-8")
       dburl._2 must beEqualTo("root")
       dburl._3 must beEqualTo("mysql")
     }
     "given default CLEARDB_DATABASE_URL property return mysql datasource" in {
       val datasource = DB.getSlickMysqlDataSource()
-      datasource.getJdbcUrl() must beEqualTo("jdbc:mysql://127.0.0.1:3306/heroku_9852f75c8ae3ea1?autoReconnectForPools=true&reconnect=true&useSSL=true&useUnicode=yes&characterEncoding=UTF-8")
+      datasource.getJdbcUrl() must beEqualTo("jdbc:mysql://127.0.0.1:3306/heroku_9852f75c8ae3ea1?useSSL=true&autoReconnectForPools=true&reconnect=true&useUnicode=yes&characterEncoding=UTF-8")
       datasource.getDriverClass() must beEqualTo("com.mysql.jdbc.Driver")
       datasource.getUser() must beEqualTo("root")
       datasource.getPassword() must beEqualTo("mysql")
