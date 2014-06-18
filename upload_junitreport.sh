a4fb3b43f56fc30246ae61977414ec0a5c3a3ce9
Frank <pussinboots666@googlemail.com>
2014-06-18 19:02:25 +0200

    - corrected unitcover api endpoint

--- upload_junitreport.sh
+++ upload_junitreport.sh
@@ -4,5 +4,8 @@ for f in $FILES
 do
   echo "Processing $f file..."
   # take action on each file. $f store current file name
-  curl -H "Content-Type:application/xml" -X POST --data-binary @$f http://localhost:9000/api/pussinboots/bankapp
+  curl -H "Content-Type:application/xml" -X POST --data-binary @$f http://unitcover.herokuapp.com/api/pussinboots/bankapp
 done
+
+curl -H "Content-Type:application/xml" -X POST -d @test-result.xml http://unitcover.herokuapp.com/api/pussinboots/bankapp
+
