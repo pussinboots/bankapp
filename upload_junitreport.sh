eadbc733e1d873f202b81dae7fa2502d5a8560cc
Frank <pussinboots666@googlemail.com>
2014-06-22 21:50:51 +0200

    - updated upload_junitreport.sh

--- upload_junitreport.sh
+++ upload_junitreport.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 owner=pussinboots
-project=bankapp
+project=banbapp
+#endpoint=localhost:9000
+endpoint=unitcover.herokuapp.com
 FILES=./target/test-reports/*
+trigger="trigger=Travis"
+travisBuildId=""
+ if [[ -n ${TRAVIS} ]]
+    then
+        echo "travis build detected"
+        travisBuildId="&travisBuildId=$TRAVIS_BUILD_ID"
+    else
+        trigger="trigger=Local"
+    fi
 #upload play junit reports
+buildnumber=$(curl -s -X POST http://$endpoint/api/$owner/$project/builds?$trigger$travisBuildId | sed -E 's/.*"buildNumber":([0-9]*).*/\1/')
+echo $buildnumber
+echo "http://$endpoint/api/$owner/$project/$buildnumber"
 for f in $FILES
 do
   echo "Processing $f file..."
-  curl -H "Content-Type:application/xml" -X POST -d @$f http://unitcover.herokuapp.com/api/$owner/$project
+  curl -H "Content-Type:application/xml" -X POST -d @$f http://$endpoint/api/$owner/$project/$buildnumber
 done
 
-curl -H "Content-Type:application/xml" -X POST -d @test-results.xml http://unitcover.herokuapp.com/api/$owner/$project
+curl -H "Content-Type:application/xml" -X POST -d @test-results.xml http://$endpoint/api/$owner/$project/$buildnumber
 
-
+curl -X POST http://$endpoint/api/$owner/$project/builds/$buildnumber/end
